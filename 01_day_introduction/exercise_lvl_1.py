49ca6d07f0ec08f0d803131703bfed98580aab15
Samuel Florez <muxanzz@gmail.com>
2023-06-28 16:59:06 -0500

    introduction exercises completed

---
+++ 01_day_introduction/exercise_lvl_1.py
@@ -0,0 +1,27 @@
+# Day 1
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+print("\n")
+
+print("Samuel")
+print("Florez")
+print("Colombia")
+print("I am enjoying 30 days of python")
+
+print("\n")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Samuel", "Python", "Bog"]))
+print(type((1, 2, 3)))
+print(type({"country":"EEUU"}))
+print(type({1, 2, 3}))
---
+++ 01_day_introduction/exercise_lvl_2.py
@@ -0,0 +1,6 @@
+a = 2.3
+b = 10.8
+
+distance = a - b
+
+print(distance)
