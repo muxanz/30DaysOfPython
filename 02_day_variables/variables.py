97ae35319d76dc97ffe2d40a99556b75d8ce1a5d
Samuel Florez <muxanzz@gmail.com>
2023-07-03 17:01:25 -0500

    working in variables

---
+++ 02_day_variables/variables.py
@@ -0,0 +1,14 @@
+# Day 2: 30 Days of Python programming
+
+first_name = "Paco"
+last_name = "Paraco"
+full_name = "Paco Paraco"
+country = "Mexico"
+city = "Tijuana"
+age = 35
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+
+brand, model, serial_number = "Samsung", "F27T350FHL", 500291
