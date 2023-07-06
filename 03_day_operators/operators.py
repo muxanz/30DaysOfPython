fc494b5f368ab0cf7bc3b8f806adf7af726f9d52
Samuel Florez <muxanzz@gmail.com>
2023-07-06 17:33:38 -0500

    working in exercises 3 day

---
+++ 03_day_operators/operators.py
@@ -0,0 +1,78 @@
+age = 35
+height = 1.75
+complex_num = 1j
+
+# 4
+# base = int(input("Enter base: "))
+# height = int(input("Enter height: "))
+# area = 0.5 * base * height
+# print("The area of the triangle is ", area)
+
+# 5
+# side_a = int(input("Enter side a: "))
+# side_b = int(input("Enter side b: "))
+# side_c = int(input("Enter side c: "))
+# perimeter = side_a + side_b + side_c
+# print(f"The perimeter of the triangle is {perimeter}")
+
+# 6
+# length = int(input("Enter length: "))
+# widht = int((input("Enter width: ")))
+# area = length * widht
+# perimeter = 2 * (length + widht)
+# print(f"The area of the rectangle is {area} and the perimeter is {perimeter}")
+
+# 7
+# radius = int(input("Enter radius: "))
+# area = 3.14 * radius * radius
+# circumference = 2 * 3.14 * radius
+# print(f"The area of the circle is {area} and the circumference is {circumference}")
+
+#11
+# x = int(input("Enter (x) value: "))
+# y = x**2 + 6*x + 9
+# print(y)
+
+#12
+print(len("python") < len("dragon"))
+
+#13
+print("on" in "python" and "on" in "dragon")
+
+#14
+print("jargon" in "I hope this course is not full of jargon.")
+
+#15
+print("on" not in "python" and "on" not in "dragon")
+
+#16
+# text_length = len("python")
+# to_float = float(text_length)
+# to_string = str(to_float)
+# print(to_string)
+# print(type(to_string))
+
+#17
+# num = int(input("Enter a number: "))
+# result = f"{num} is even" if num % 2 == 0 else f"{num} is not even"
+# print(result) 
+
+#18
+print(7 // 3)
+print(int(7 / 3))
+
+#19
+print(type("10") == type(10))
+
+#20
+print(int(9.8) == 10)
+
+#21
+# hours = int(input("Enter hours: "))
+# rate = int(input("Enter rate per hour: "))
+# print(f"Your weekly earning si {hours * rate}")
+
+#22
+years = int(input("Enter number of years you have lived: "))
+seconds_per_year = 31536000
+print(f"You have lived for {years * seconds_per_year} seconds.")
